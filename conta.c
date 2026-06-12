85fc3683918f87f363a4e05b6a5e9832b1f1fd57
ThainaClaro <clarotay@gmail.com>
2026-06-12 16:56:36 -0300

    5 anos estudando programação e até hoje não sei fazer um menu descente enfim...

--- conta.c
+++ conta.c
@@ -4,17 +4,16 @@
 
 
 void main(){
-	bool loop = true;
-	   printf("Boa tarde");
-
-  while(loop);
+    int saldo = 1000;
+    bool loop = true;
     int op;
-    float valor;
-    float saque;
-    float emp;
-             printf("Digite um numero para processeguir:\n(1)Verificar Saldo\n(2)Depositar valor\n(3)Sacar Valor\n(4)Pedir Emprestimo\n(5)Pagar Emprestimo\n(6)Consultar emprestimo\n(7) sair \n ");
+    int valor;
+    int saque;
+    int emp;
+             printf("Digite um numero para processeguir:\n(1)Verificar Saldo\n(2)Depositar valor\n(3)Sacar Valor\n(4)Pedir Emprestimo\n(5)Pagar Emprestimo\n(6)Consultar emprestimo\n(7)sair \n");
 	      scanf("%d",&op);
-	 float saldo = 1000;
+
+    while(loop){
         switch(op){
 		case 1:
 		       	printf("Seu saldo atual é:%d \n",saldo);
@@ -23,27 +22,27 @@ void main(){
 	        case 2: 
 			 printf("Digite o valor que deseja depositar: ");
 			 scanf("%d",&valor);
-			 valor + saldo;
+			 saldo = valor + saldo;
 			 printf("Depositado com sucesso\n Seu saldo atual é de:%d \n",saldo);
 			 loop = false;
 			 break;
 		case 3:
 			 printf("Digite o valor que deseja sacar: ");
 			scanf("%d",&saque);
-			saldo - saque;
+			saldo = saldo - saque;
 			printf("Saque realisado com sucesso\n Seu saldo atual é de: %d \n",saldo);
 			loop = false;
 			break;
 	       case 4:
 		       printf("Digite o valor do emprestimo: ");
 		       scanf("%d",&emp);
-		       emp + saldo;
+		       saldo = emp + saldo;
 		       printf("emprestimo realizado com sucesso\n Seu saldo atual é de: %d \n",saldo);
 		       loop= false;
 		       break;
 	       case 5:
 		       printf("O valor a ser quitado é de: %d \n",emp);
-		       saldo - emp;
+		       saldo = saldo - emp;
 		       printf("Emprestimo pago com secesso\n Seu saldo atual é de : %d \n",saldo);
 		       loop = false;
 		       break;
